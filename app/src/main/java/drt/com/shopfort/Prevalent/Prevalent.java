69e8266d5cbcf6e48b77e98a8144bcfa4d71a8f0
shyamsingh7 <singhshyam578@gmail.com>
2019-06-03 20:48:22 -0700

    initial commit

---
+++ app/src/main/java/drt/com/shopfort/Prevalent/Prevalent.java
@@ -0,0 +1,12 @@
+package drt.com.shopfort.Prevalent;
+
+
+import drt.com.shopfort.Model.Users;
+
+public class Prevalent
+{
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
---
+++ app/src/main/java/drt/com/shopfort/ViewHolder/CartViewHolder.java
@@ -0,0 +1,34 @@
+package drt.com.shopfort.ViewHolder;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import drt.com.shopfort.Interface.ItemClickListner;
+import drt.com.shopfort.R;
+
+public class CartViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
+{
+    public TextView txtProductName, txtProductPrice, txtProductQuantity;
+    private ItemClickListner itemClickListner;
+
+
+    public CartViewHolder(View itemView)
+    {
+        super(itemView);
+
+        txtProductName = itemView.findViewById(R.id.cart_product_name);
+        txtProductPrice = itemView.findViewById(R.id.cart_product_price);
+        txtProductQuantity = itemView.findViewById(R.id.cart_product_quantity);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        itemClickListner.onClick(view, getAdapterPosition(), false);
+    }
+
+    public void setItemClickListner(ItemClickListner itemClickListner)
+    {
+        this.itemClickListner = itemClickListner;
+    }
+}
